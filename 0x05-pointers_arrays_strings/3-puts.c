3b2bcb460aab04aa5054af280ff67d6d68ba6785
Lpharome <lpharome@gmail.com>
2023-02-27 05:09:02 -0800

    correction of 3put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <string.h>
 /**
  * _puts - minic the stdio version of puts
  * @str: the value to process to the stdout.
  */
-
 void _puts(char *str)
 {
-	while (*str != '\0')
+	while (*str > 0)
 	{
-		_putchar(*str++);
-	       
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
