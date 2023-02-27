72f2b77cf1efa03ef59f23e01d82267b78c2ce7a
Lpharome <lpharome@gmail.com>
2023-02-27 14:59:58 -0800

    ./100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,48 @@
 #include "main.h"
-#include <stdio.h>
- 
 /**
- * _atoi - converts a string to an integer.
- * @s: the string to convert
- * Return: the converted string.
- */
+  * _atoi- convert a string to an integer.
+  * @s: string input
+  * Return: int number
+**/
 int _atoi(char *s)
 {
-	short boolean;
-	int i, minus, result;
+	int number2 = 0, size = 0, negatives = 0, p = 1, j, start = 0, end;
+	unsigned int number = 0;
 
-	i = minus = result = boolean = 0;
-	minus = -1;
-
-	while (s[i] != '\0')
+	if (s[0] == '\0')
+	{ return (number2); }
+	while (s[size] != '\0')
 	{
-		if (s[i] == '-')
-			minus *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result -= (s[i] - '0');
-			boolean = 1;
-		}
-		else if (boolean == 1)
-			break;
-		i++;
+	size++;
 	}
-	result *= minus;
-	return (result);
+	while (s[start] < 48 || s[start] > 57)
+	{
+	start++;
+		if (start == size)
+		{ return (number2); }
+	}
+	end = start;
+	while (s[end + 1] > 47 && s[end + 1] < 58)
+	{
+	end++;
+	}
+	for (j = 0; j < start; j++)
+	{
+	if (s[j] == 45)
+	{ negatives++; }
+	}
+	for (j = end; j >= start; j--)
+	{
+	number = number + (s[j] - '0') * p;
+	if (p < 1000000000)
+	{ p = p * 10; }
+	}
+	if (number == 2147483648 && (negatives % 2 != 0))
+	{ number2 = -2147483648; }
+	else
+	{ number2 = number;
+	if (negatives % 2 != 0)
+	{ number2 = -number2; }
+	}
+return (number2);
 }
