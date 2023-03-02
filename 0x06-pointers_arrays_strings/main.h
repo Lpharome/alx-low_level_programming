db56de4ca53479fab7335b5cc0ad58a466b2b9ee
Lpharome <lpharome@gmail.com>
2023-03-02 04:17:39 -0800

    last push

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,6 @@ char *string_toupper(char*);
 char *cap_string(char*);
 char *rot13(char*);
 void print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *rot13(char *);
